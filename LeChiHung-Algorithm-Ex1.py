7ed67b467bb909e7797b9ec31a568e4b6284b00a
Hunglechi <108796607+Hunglechi@users.noreply.github.com>
2022-10-26 17:10:47 +0700

    add new file (#1)
    * add new file
    
    * add more file
    
    Co-authored-by: LeChiHung <hunglechi.dev@gmail.com>

---
+++ LeChiHung-Algorithm-Ex1.py
@@ -0,0 +1,23 @@
+def sumInteger(n):
+    S = 0
+    sum = 0
+    dem = -1
+    for i in range(1, n + 1):
+        sum = sum + i
+        S = S + (-1) ** (i + 1) / sum
+    return S
+
+
+while True:
+    try:
+        n = int(input("Enter a positive integer number: "))
+        if n < 0:
+            raise Exception
+        result = sumInteger(n)
+        print(f"The sum of function is: ")
+        print(result)
+        break
+    except ValueError:
+        print("The number must be a positive number.")
+    except Exception:
+        print("The number must be positive.")
---
+++ Lê Chí Hùng-Algorithm-Ex2.py
@@ -0,0 +1,27 @@
+start = int(input("start: "))
+delta = int(input("delta: "))
+row = int(input("row: "))
+
+def sumPart(n):
+    du=0
+    sum=0
+    while (n>0):
+        du=int(n%10)
+        n=int(n/10)
+        sum=sum+du
+    return sum
+
+array=[]
+start=sumPart(start)
+print(start)
+for i in range(0,row-1):
+    for j in range(0,i+2):
+        start=start+delta
+        if(start<=9):
+            print(start,end=" ")
+        else:
+            start=sumPart(start)
+            print(start,end=" ")
+    print()
+
+
