9025c85108d14fa1f275b5a69bb40d12c840bfd3
LeChiHung <hunglechi.dev@gmail.com>
2022-10-26 23:06:32 +0700

    add new information

--- Shapes.py
+++ Shapes.py
@@ -4,4 +4,7 @@ class Shapes(object):
     """
         This class is used as utils to calculate properties of all shapes
     """
-    
+
+    @staticmethod
+    def calculate_the_area_of_a_rectangle(length, width):
+      return length * width
